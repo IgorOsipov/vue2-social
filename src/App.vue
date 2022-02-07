ff484e50c9cb2135ca2573b1ad38d2d8ddacad26
Ihor Osypov <ihor.osypov@geeksforless.com>
2022-02-07 19:46:32 +0200

    Added NavBar

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,18 @@
 <template>
   <div>
+    <main-nav />
     <router-view />
   </div>
 </template>
+
+<script>
+import NavBar from "./components/NavBar";
+
+export default {
+  components: {
+    "main-nav": NavBar,
+  },
+};
+</script>
 
 <style></style>
